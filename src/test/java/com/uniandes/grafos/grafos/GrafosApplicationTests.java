ca924f8331d0180086da1be28ebcf4c7127d57e5
Reinaldo Portocarrero <juniorportom@gmail.com>
2020-03-18 22:39:58 -0500

    Base proyecto grafos

---
+++ src/test/java/com/uniandes/grafos/grafos/GrafosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniandes.grafos.grafos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
